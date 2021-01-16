8d1fb0e06795c68a61fa3a99f6658fe8ecce1132
Niraj Regmee <n.regmee19@gmail.com>
2021-01-16 03:06:14 -0600

    fixed an issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import UnauthorizedLayout from './Layouts/UnauthorizedLayout'
-import logo from './logo.svg';
+
 import './App.css';
 
 
